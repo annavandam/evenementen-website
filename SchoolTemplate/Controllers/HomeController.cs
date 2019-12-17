2d20e2ae4b68a4232f35fdaf98913dc7e54f0908
Anna van Dam <110274@st-maartenscollege.nl>
2019-12-17 13:53:02 +0100

    Anna wachtwoord en leerlingnummer sql nog een keer

--- SchoolTemplate/Controllers/HomeController.cs
+++ SchoolTemplate/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace SchoolTemplate.Controllers
   public class HomeController : Controller
   {
     // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
-    string connectionString = "Server=172.16.160.21;Port=3306;Database=;Uid=110274;Pwd=rairLefs;";
+    string connectionString = "Server=172.16.160.21;Port=3306;Database=110274;Uid=110274;Pwd=rairLefs;";
 
     public IActionResult Index()
     {
